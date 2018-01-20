960679d5f7dce1b792c8ee2fe62b7512240a5885
KMermi <KMermi@KMERMI-NBK1.ip-soft.net>
2018-01-20 13:18:32 -0600

    Verison 2.0.1 is ready for your reviews

---
+++ MavenWithSeleniumWebDriver/src/com/codistan/qaclass1/maven/TestWithMaven.java
@@ -0,0 +1,21 @@
+package com.codistan.qaclass1.maven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestWithMaven {
+
+	@Test
+	public void amazonTest() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\KMermi\\Documents\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]")).sendKeys("fidget"+Keys.ENTER);
+	}
+
+}
